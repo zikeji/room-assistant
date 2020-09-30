0d492dda373e044b2cd1ffa9a459cb9c655ae4d6
Zikeji <me@zikeji.com>
2020-09-30 18:44:39 -0400

    fix(home-assistant): use HA's regex for validation and in makeId. add foreign character test

--- src/util/id.spec.ts
+++ src/util/id.spec.ts
@@ -1,7 +1,7 @@
 import { makeId } from './id';
 
 describe('makeId', () => {
-  const idRegex = /^[a-z1-9-]+$/;
+  const idRegex = /^[a-zA-Z0-9_-]+$/;
 
   it('should convert inputs to lower case', () => {
     expect(makeId('ABC-def123')).toMatch(idRegex);
@@ -11,11 +11,11 @@ describe('makeId', () => {
     expect(makeId('ble-84:87:ad:ef')).toMatch(idRegex);
   });
 
-  it('should convert underscores to dashes', () => {
-    expect(makeId('bluetooth-classic_129-1-1')).toMatch(idRegex);
-  });
-
   it('should convert apostrophes to dashes', () => {
     expect(makeId("parent's-room")).toMatch(idRegex);
+  });
+
+  it('should remove foreign characters', () => {
+    expect(makeId('测试-房间')).toMatch(idRegex);
   });
 });
